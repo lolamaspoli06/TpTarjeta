9ebfe858e0bc1c9959294b0ee1bf70acf8d18e8b
lolamaspoli06 <lolamaspoli2@gmail.com>
2024-10-01 17:19:07 -0300

    Add files via upload

---
+++ boleto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tp2AAT
+{
+    public class Boleto
+    {
+        public string Linea { get; private set; }
+        public decimal SaldoRestante { get; private set; }
+        public DateTime FechaHora { get; private set; }
+    
+        public Boleto(decimal saldoRestante)
+        {
+            this.SaldoRestante = saldoRestante;
+            this.FechaHora = DateTime.Now;
+        }
+    
+        public override string ToString()
+        {
+            return $"Boleto - Saldo Restante: {SaldoRestante:C}, Fecha y Hora: {FechaHora}";
+        }
+    }
+}

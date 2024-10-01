0c8e8168f4c35fffe6ecf86cd2f66ba47bb615a6
lolamaspoli06 <lolamaspoli2@gmail.com>
2024-10-01 17:17:43 -0300

    Add files via upload

---
+++ colectivo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tp2AAT
+{
+    public class Colectivo
+    {
+    
+        public Boleto PagarCon(Tarjeta tarjeta)
+        {
+            if (tarjeta.DescontarPasaje())
+            {
+                return new Boleto( tarjeta.Saldo);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

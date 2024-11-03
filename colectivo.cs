023f3cd53d353bd46ba17badfd2fd3c4ebbf28bd
lolamaspoli06 <lolamaspoli2@gmail.com>
2024-11-03 15:45:12 -0300

    Iteracion 1

---
+++ colectivo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using BoletoNamespace;
+using TarjetaNamespace;
+namespace ColectivoNamespace;
+
+public class Colectivo
+{
+
+    public Boleto PagarCon(Tarjeta tarjeta)
+    {
+        if (tarjeta.DescontarPasaje())
+        {
+            return new Boleto(tarjeta.Saldo);
+        }
+        else
+        {
+            return null;
+        }
+    }
+}

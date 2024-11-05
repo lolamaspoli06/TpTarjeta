38183e46356c9ed0698c79324e0f6832ce7e45f9
lolamaspoli06 <lolamaspoli2@gmail.com>
2024-11-05 16:58:19 -0300

    Add files via upload

--- boleto.cs
+++ boleto.cs
@@ -1,27 +1,29 @@
-
+﻿
 using System;
 using System.Collections.Generic;
 
-namespace BoletoNamespace;
-
-public class Boleto
+namespace BoletoNamespace
 {
-    public DateTime Fecha { get; private set; }
-    public string TipoTarjeta { get; private set; }
-    public string LineaColectivo { get; private set; }
-    public decimal TotalAbonado { get; private set; }
-    public decimal SaldoRestante { get; private set; }
-    public string IdTarjeta { get; private set; }
-    public string DescripcionExtra { get; private set; }
-  
-    public Boleto(DateTime fecha, string tipoTarjeta, string lineaColectivo, decimal totalAbonado, decimal saldoRestante, string idTarjeta, string descripcionExtra)
+
+    public class Boleto
     {
-        Fecha = fecha;
-        TipoTarjeta = tipoTarjeta;
-        LineaColectivo = lineaColectivo;
-        TotalAbonado = totalAbonado;
-        SaldoRestante = saldoRestante;
-        IdTarjeta = idTarjeta;
-        DescripcionExtra = descripcionExtra;
+        public DateTime Fecha { get; private set; }
+        public string TipoTarjeta { get; private set; }
+        public string LineaColectivo { get; private set; }
+        public decimal TotalAbonado { get; private set; }
+        public decimal SaldoRestante { get; private set; }
+        public string IdTarjeta { get; private set; }
+        public string DescripcionExtra { get; private set; }
+
+        public Boleto(DateTime fecha, string tipoTarjeta, string lineaColectivo, decimal totalAbonado, decimal saldoRestante, string idTarjeta, string descripcionExtra)
+        {
+            Fecha = fecha;
+            TipoTarjeta = tipoTarjeta;
+            LineaColectivo = lineaColectivo;
+            TotalAbonado = totalAbonado;
+            SaldoRestante = saldoRestante;
+            IdTarjeta = idTarjeta;
+            DescripcionExtra = descripcionExtra;
+        }
     }
 }

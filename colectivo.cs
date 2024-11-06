07d190d0598845b76bf8248d541e085ede9e14ca
lolamaspoli06 <lolamaspoli2@gmail.com>
2024-11-05 21:15:24 -0300

    Add files via upload

--- colectivo.cs
+++ colectivo.cs
@@ -100,7 +100,7 @@ namespace ColectivoNamespace
 
         private bool EsFranquicia(Tarjeta tarjeta)
         {
-            return tarjeta is MedioBoleto || tarjeta is Tarjeta.BoletoGratuito; // Agrega otras franquicias si tienes más
+            return tarjeta is MedioBoleto || tarjeta is BoletoGratuito || tarjeta is BoletoGratuitoJubilados; 
         }
 
         private bool HorarioPermitido(DateTime fecha)
